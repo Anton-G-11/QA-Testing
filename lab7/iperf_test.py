b941baaed394e8a1cd426eb94bebfc8bbb1fbf39
Anton-G-11 <81290212+Anton-G-11@users.noreply.github.com>
2023-01-22 10:48:30 +0200

    Add files via upload

---
+++ lab7/iperf_test.py
@@ -0,0 +1,19 @@
+import parser
+from confest import client, server
+import sys
+import logging
+
+class TestSuite():
+
+    def test_iperf3_server_connection(self, server):
+        stderr = server
+        assert stderr
+    
+    def test_iperf3_client_connection(self, client):
+        stdout, error= client
+        print ("    >Received form fixture client is: {}".format(stdout))
+        assert not error
+        dict = parser_1.parser(stdout.decode())
+        for value in dict:
+            assert value['Transfer']> 10 and value["Bitrate"] > 80
+        
